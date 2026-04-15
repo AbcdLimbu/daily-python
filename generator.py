01cf8b3df5272f92473865dda92f2f0db5b76287
AbcdLimbu <pokhrajlimbu2004>
2026-04-15 22:00:27 +0530

    quick challenge

--- generator.py
+++ generator.py
@@ -1,10 +1,8 @@
-def count():
-    for i in range(1, 10):
-        return i
+# create a generator that prints 0, 2, 4, 6, 8,...,10
 
-def numbers():
-    for i in range(1, 10):
+def even():
+    for i in range(1, 10, 2):
         yield i
-
-num = numbers()
-print(next(num))
+        
+for num in even():
+    print(num)
